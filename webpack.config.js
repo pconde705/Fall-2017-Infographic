f1fcaa5ed0821ed50c9524b7c2792ac4c2261d3f
Patrick Conde <patrick.conde.costas@gmail.com>
2017-12-07 10:12:33 -0800

    Changed upload settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,5 @@
+
 var path = require("path");
-var webpack = require("webpack");
-
-var plugins = [];
-var devPlugins = [];
-
-var prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
 
 module.exports = {
   context: __dirname,
@@ -27,19 +7,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js"
-  },
-  plugins: plugins,
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
   },
   devtool: 'source-map',
   resolve: {
